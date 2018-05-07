02e4f5e607f8ad675e7cba0d5a7bd5c5d5ce399d
Quentin <qbrogly@gmail.com>
2018-05-06 18:10:13 -0700

    Finish Adopt page

--- src/components/Adopt.js
+++ src/components/Adopt.js
@@ -14,21 +14,6 @@ class Adopt extends Component {
                         <p>
                         The Los Angeles Department of Animal Services offers a wide range of services that fulfill its mission to <b>“Promote and Protect the Health, Safety and Welfare of Animals and People in the City of Los Angeles.”</b>
                         </p>
-                        <p>Find information about :</p>
-                        <ul>
-                            <li>Adopting a pet</li> 
-                            <li>Finding a lost pet</li>
-                            <li>How to license your dog or horse</li>
-                            <li>Animal vaccinations</li>
-                            <li>Spay & neuter information</li> 
-                            <li>Microchipping</li> 
-                            <li>How to file a barking dog</li> 
-                            <li>Definitions of animal cruelty and how to report it</li>
-                            <li>Dangerous animal complaint</li> 
-                            <li>What to do if you find a stray animal</li> 
-                            <li>How to arrange for a dead animal to be picked up</li>
-                            <li>Permit information</li> 
-                        </ul>
                         <p>As the needs of the community grow, so do our services. Please check back often to see what new services and information that the Department is offering!
                         </p>
                     </div>
@@ -72,6 +57,56 @@ class Adopt extends Component {
                             10 days or more pets: $63.50 or $83.50 with license for dogs available for adoption 10 days or more and $50.50 for cats available for adoption 10 days or more
                             </p>
                         </div>
+                    </div>
+                    <div className="adopt__seniors">
+                        <h1>Seniors</h1>
+                        <h3>Seniors for seniors</h3>
+                        <p>
+                        LA Animal Services Seniors for Seniors program matches senior companion animals (seven years and older) from Los Angeles Animal Shelters with senior adults (62 years and older). Senior adopters receive 50% off the adoption fee which includes free microchipping.
+                        </p>
+                        <p> See adoption fees</p>
+                        <p>
+                        The program is available at each of the City’s Animal Shelters and at mobile adoption sites.
+                        </p>
+                        <p>
+                        Some seniors (62 years and older) living in the City of Los Angeles may alsoSenior with dog qualify to receive a $10.00 dog license.
+                        </p> 
+                        <p>Please bring your low income/lifeline DWP bill to qualify.
+                        </p>
+                        <p>For more information about $10.00 dog licenses for seniors, click here, or call the Department of Animal Services Licensing Division at 213-482-9550.
+                        </p>
+                        <h4><a href="http://www.laanimalservices.com/wp-content/uploads/2011/06/10-Health-Reasons-Why-companion-animals-Are-Great-For-Seniors.pdf" target="_blank">10 Health Reasons Why companion animals Are Great For Seniors</a></h4>
+                        <h3>Discounted dog licence</h3>
+                        <p>
+                        Qualifications for a $10.00 Dog License:
+                        </p>
+                        <p>
+                        In order to qualify for a $10.00 dog license, the applicant must either be 62 years of age or older, or disabled, and meet low income requirements. 
+                        </p>
+                        <p>For either seniors or the disabled, the following must be submitted</p>
+                        <ul>
+                            <li>A spay or neuter certificate or evidence from a veterinarian that the dog is incapable of breeding, or a signed statement agreeing to have the dog spay or neuter within 90 days.</li>
+                            <li>A current rabies vaccination certificate for the dog.</li>
+                            <li>Low Income Benefit Application for All – English, Spanish</li>
+                        </ul>
+                        <p>
+                        A disabled individual may qualify for one $10.00 dog license, pursuant to Los Angeles Municipal Code Section 53.15 (f). </p>
+                        <p>To qualify, the disabled individual must provide to the Department of Animal Services a signed statement from a licensed physician certifying that he/she is disabled and meet low income requirements. 
+                        </p>
+                        <p>Per the Municipal Code: ”An individual shall be considered disabled if he or she is unable to engage in any substantial, gainful activity by reason of a medically determinable physical or mental impairment, which can be expected to result in death or to be of long, continued and indefinite duration.”
+                        </p>
+                        <p>
+                        Please submit the requested information and Low Income Benefit Application to:</p>
+                        <p>
+                        Attn: Discount Dog License Application
+                        Department of Animal Services
+                        221 N. Figueroa Street
+                        6th Floor, Suite 600
+                        Los Angeles, CA 90012
+                        </p>
+                        <p>
+                        For more information about $10.00 dog license for seniors or the disabled, call the Department of Animal Services Licensing Division at 213-482-9550.
+                        </p>
                     </div>
                 </div>
                 <Footer />
